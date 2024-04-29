fa07d7c5020cabbf3f4deab4127aa3906936c0ec
hwiseong18 <leehs050725@naver.com>
2024-04-29 20:38:49 +0900

    111

---
+++ CAT-CERT_WEB_3/PHP_variables.php
@@ -0,0 +1,11 @@
+<?php
+    $int = 12345;
+    $char = "A";
+    %string = "Hello PHP";
+    $float = 12.345;
+    $array = array("array1", "array2", "array3");
+
+    //PHP 문자열과 변수는 .을 통해 두 수식을 이을 수 있다.
+
+    
+?>
